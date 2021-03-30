354e0c01a444625b317b905c3b887896dad61f83
Sayuri <lilium.mentha@gmail.com>
2021-03-30 18:50:02 +0900

    ダミー翻訳追加

--- app/src/main/java/com/example/tg_test/MainActivity.java
+++ app/src/main/java/com/example/tg_test/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.tg_test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipboardManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,14 +15,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
- //       String textbox = getText(findViewById(R.id.textbox));
- //       String textbox2 = getText(findViewById(R.id.textbox2));
- //       textbox2 = "あいうえお" + textbox
 
-         String textbox = (String) getText(findViewById(R.id.textbox));
-         String textbox2 = (String) getText(findViewById(R.id.textbox2));
-         textbox2 = "あいうえお" + textbox2;
+        TextView tBox = (TextView) findViewById(R.id.textbox);
+        TextView tBox2 = (TextView) findViewById(R.id.textbox2);
+        Button change = (Button) findViewById(R.id.changeButton);
+        Button copy = (Button) findViewById(R.id.copyButton);
 
-//aaaaaaa
+        change.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String str1 = String.valueOf(tBox.getText());
+                tBox2.setText("「いろはにほへと」" + str1 + "「ゑひもせず」");
+
+            }
+        });
+
+        /*
+        copy.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                ClipboardManager copiedText = (ClipboardManager) tBox2.getSystemService
+
+            }
+        });
+
+         */
+
+
+
+
     }
 }
