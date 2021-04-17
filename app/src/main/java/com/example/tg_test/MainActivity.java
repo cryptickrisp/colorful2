2690d92696f914d765949f42d490a8c1d9678f75
Kris Popendorf <krisp@krisp.jp>
2021-04-17 09:22:05 +0900

    add translate dependency

--- app/src/main/java/com/example/tg_test/MainActivity.java
+++ app/src/main/java/com/example/tg_test/MainActivity.java
@@ -70,10 +70,43 @@ public class MainActivity extends AppCompatActivity {
         });
 
          */
+    }
 
-        
+    // https://medium.com/@yeksancansu/how-to-use-google-translate-api-in-android-studio-projects-7f09cae320c7
+    public void getTranslateService() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
+        try (InputStream is = getResources().openRawResource(R.raw.credentials)) {
+            //Get credentials:
+            final GoogleCredentials myCredentials = GoogleCredentials.fromStream(is);
 
+            //Set credentials and get translate service:
+            TranslateOptions translateOptions = TranslateOptions.newBuilder().setCredentials(myCredentials).build();
+            translate = translateOptions.getService();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
 
+    public void translate() {
+        //Get input text to be translated:
+        originalText = inputToTranslate.getText().toString();
+        Translation translation = translate.translate(originalText, Translate.TranslateOption.targetLanguage("tr"), Translate.TranslateOption.model("base"));
+        translatedText = translation.getTranslatedText();
+
+        //Translated text and original text are set to TextViews:
+        translatedTv.setText(translatedText);
+    }
+
+    public boolean checkInternetConnection() {
+        //Check internet connection:
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        //Means that we are connected to a network (mobile or wi-fi)
+        connected = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED;
+
+        return connected;
     }
 }
